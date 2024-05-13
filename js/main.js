6c1e864f3d9b6c848981528066655e2671098d60
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-13 23:43:39 +0530

    2. click event (ouch!🤕)

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 
 const myBox = document.getElementById("myBox");
 
-
+myBox.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = "red";
+    event.target.textContent = "ouch!🤕";
+})
 
 
 
