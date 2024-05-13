6576b230cae9feabe7e69916092eb73b67d497c8
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-13 23:48:35 +0530

    3. mouseover event (don't do it😲)

--- js/main.js
+++ js/main.js
@@ -4,9 +4,13 @@ const myBox = document.getElementById("myBox");
 
 myBox.addEventListener("click", (event) => {
     event.target.style.backgroundColor = "red";
-    event.target.textContent = "ouch!🤕";
+    event.target.textContent = "ouch🤕";
 })
 
+myBox.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "yellow";
+    event.target.textContent = "don't do it😲";
+})
 
 
 
